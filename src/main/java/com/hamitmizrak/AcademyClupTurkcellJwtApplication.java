4aed9001d20e84e89ad28a7449bf8896fadeb9ed
hamitmizrak <hamitmizrak@gmail.com>
2025-10-03 11:20:28 +0300

    Shelling

--- src/main/java/com/hamitmizrak/AcademyClupTurkcellJwtApplication.java
+++ src/main/java/com/hamitmizrak/AcademyClupTurkcellJwtApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AcademyClupTurkcellJwtApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AcademyClupTurkcellJwtApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AcademyClupTurkcellJwtApplication.class, args);
+    }
 
 }
